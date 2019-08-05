085e4cf85eee858b5437295f6b5468589a5e5d7c
Marei Peischl <marei@peitex.de>
2019-08-05 09:11:05 +0200

    adjusting version number to 1.01

--- addlicense.sh
+++ addlicense.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd tex
+fileversion="1.01"
+filedate=$(date '+%Y/%m/%d')
+echo $filedate
 for filename in *.sty *.cls *.def *.cfg
 do
     echo $filename
-    (sed "s/<FILENAME>/$filename/" ../LICENSE_info && cat $filename ) >> tmp_license.txt
+    (sed  "s/<FILENAME>/$filename/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" ../LICENSE_info && cat $filename ) >> tmp_license.txt
     mv tmp_license.txt $filename
 done
 cd  ..
@@ -12,7 +15,7 @@ cd example
 for filename in *.tex *.bib *.lco
 do
     echo $filename
-    (sed "s/<FILENAME>/$filename/" ../LICENSE_info-demo && cat $filename ) >> tmp_license.txt
+    (sed "s/<FILENAME>/$filename/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" ../LICENSE_info-demo && cat $filename ) >> tmp_license.txt
     mv tmp_license.txt $filename
 done
 cd ..
