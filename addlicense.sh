2d3e4249ae26e8071bfbf8867bf1d424f2d13cee
Marei (peiTeX) <marei@peitex.de>
2020-09-19 13:14:00 +0200

    restructure license info

--- addlicense.sh
+++ addlicense.sh
@@ -3,10 +3,10 @@ cd tex
 fileversion="3.03"
 filedate=$(date '+%Y/%m/%d')
 echo $filedate
-for filename in *.sty *.cls *.def *.cfg *.clo
+for filename in *.sty *.cls *.def *.cfg *paper.clo
 do
     echo $filename
-    (sed  "s/<FILENAME>/$filename/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" ../LICENSE_info && cat $filename ) >> tmp_license.txt
+    (sed  "s/<FILENAME>/$filename/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" ../LICENSE_info && cat $filename) >> tmp_license.txt
     mv tmp_license.txt $filename
 done
 cd  ..
@@ -15,8 +15,15 @@ cd example
 for filename in *.tex *.bib *.lco
 do
     echo $filename
-    (sed "s/<FILENAME>/$filename/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" ../LICENSE_info-demo && cat $filename ) >> tmp_license.txt
+    (cat ../LICENSE_info-demo && cat $filename ) >> tmp_license.txt
     mv tmp_license.txt $filename
 done
 cd ..
 
+
+for filename in {tex/*.{sty,cls,def,cfg,clo},example/*.{tex,bib,lco}}
+do
+    echo $filename
+    (sed  "s/<FILENAME>/$(basename $filename)/; s/<FILEDATE>/$(date '+%Y\/%m\/%d')/; s/<FILEVERSION>/$fileversion/" FILEINFO_header && cat $filename) >> tmp_license.txt
+    mv tmp_license.txt $filename
+done
