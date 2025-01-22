5a82c2b1c61fa70f9d9d73bb093998aa892e5767
Marei (peiTeX) <marei@peitex.de>
2025-01-22 11:14:20 +0100

    fix logo bundle script for gitlab auto deployment

--- tools/logo-installation/build_logo_bundle.sh
+++ tools/logo-installation/build_logo_bundle.sh
@@ -6,10 +6,10 @@ mecheng_bundle_name="TUDa_mecheng_logo_bundle"
 
 rm -rf "$bundle_name" "$mecheng_bundle_name"
 mkdir -p "$bundle_name"
-cp -- "$logo_dir/tuda_logo.pdf" *.bat *.lua "$bundle_name" README.md "$bundle_name/."
+cp -- "$logo_dir/tuda_logo.pdf" *.bat *.lua README.md "$bundle_name/."
 zip -r "$bundle_name.zip" "$bundle_name"
 
 cp -r "$bundle_name" "$mecheng_bundle_name"
-cp -r "$logo_dir/tuda_maschinenbau*.pdf" "$mecheng_bundle_name/."
+cp "$logo_dir/"tuda_maschinenbau*.pdf "$mecheng_bundle_name/."
 sed -i "s/# TU Darmstadt Logo Bundle/# TU Darmstadt Logo Bundle \(including mecheng logos\)/g" "$mecheng_bundle_name/"*.md
 zip -r "$mecheng_bundle_name.zip" "$mecheng_bundle_name"
